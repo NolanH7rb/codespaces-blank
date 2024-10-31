da2b34cf16719d83432582f0984af8ff6c75f174
NolanH7rb <nhall255@boyslatin.net>
2024-10-31 19:17:51 +0000

    Started Javascript Fundamentals

---
+++ Unit 2/jsPractive.js
@@ -0,0 +1,66 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Data types are the basic building
+// block of Javascript programs
+
+//Strings
+// strings are a piece of dat that is wrapped in
+// qutation marks.
+
+"double quotes tell the computer that this is a string."
+"My name is Nolan"
+"309090"
+"1!#$!#$"
+'singe quotes can also be used tow rite strings. '
+console.log(2+5);
+console.log("This is some text...")
+
+// Intergers
+// Intergers are pieces of data that represent numbers.
+23
+345
+1023
+3209472095702970
+console.log(3*12)
+
+// Booleans
---
+++ jsFoundation.js
@@ -0,0 +1,33 @@
+// data types = most fundamental building block
+// of JS programming language.
+
+// String data type - are characters bound together
+// with quotation marks
+"anything wrapped in quotes"
+"it can be numbers, letters, or symbols"
+"we can create strings with the double quotation marks."
+'we can also write strings with single quotation marks.'
+'yes, id like to make $85,000 - 804,000 a year starting...'
+console.log(85000- 104000)
+
+// Interger data type- numbers
+// doesn't matter if its a whole or decimal number
+14313414314
+7656
+354.877544363
+0.34664
+
+// Boolean data type - true or false value.
+true
+false
+
+// variables - containers for data
+// we create variables by doing the following:
+// 1. we need to delcare the type of variable we are using.
+// 2. we need to give the variable a name.
+// 3. we need to give it an equal sign.
+// 4. we need to give it data/ a value.
+var weather_city = 'philly' // universal variable keyword.
+let weather_temp = 70 // temporary/ expected to change.
+let forcast_weekly = 'sunny' // constant/ doesnt change.
+const main_cityweather = 'Philly'
