39c436da75ad9a77ea82bcda9cc3845d5a16ffc6
NolanH7rb <nhall255@boyslatin.net>
2024-11-04 18:56:22 +0000

    async work november 1 finished up on nov 4.

---
+++ Unit3_javascript/ asyncActivity_nov1.js
@@ -0,0 +1,25 @@
+//Describe the difference between var, let, and const. Your answer can be formatted as a comment or a string.
+//var and let basically mean the number or topic can change and const basically mean it won't change and it is most likely to stay the same.
+
+//Use what you've learned about data types and variables and create three variables that when multiplied together will result in the value 4000. Research the console.log() function on w3schools to output your result in the terminal. 
+let a= 2
+let b= 2000
+console.log(a*b)
+
+let c= 1000
+let d= 1
+let e= 4
+console.log(c*d*e)
+
+let f= 200
+let g= 5
+let h= 4
+console.log(f*g*h)
+
+//Describe a scenario where you would use the let variable and when you would use a const variable in a computer program. Your answer can be formatted as a comment or a string.
+// A time when you can use the let variable is when you are trying to create a weather website program in philadelphia the temperature outside would be undfer the let variable. While the place which is "philly" would be under the const variable because we're only trying to keep track of the weather in Philadelphia.
+
+//If there were three things you that feel could improve the class, what would those things be and why? Your answer can be formatted as a comment or a string.
+//Turning in work on time.
+//staying on focus throughout the class.
+//turning work in on time.
