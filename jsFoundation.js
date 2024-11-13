d50ff7bc26c1dfa14c46cb2a18e8c2169bd3458b
NolanH7rb <nhall255@boyslatin.net>
2024-11-13 20:15:52 +0000

    reviewed operators

--- jsFoundation.js
+++ jsFoundation.js
@@ -31,3 +31,43 @@ var weather_city = 'philly' // universal variable keyword.
 let weather_temp = 70 // temporary/ expected to change.
 let forcast_weekly = 'sunny' // constant/ doesnt change.
 const main_cityweather = 'Philly'
+
+var example1 = 'this is a string' // anything wrapped in quotes.
+var example2 = true // boolean = true and false.
+var example3 = 1234567 // intergers = numbers.
+
+// var is short for variable-
+// it is the universal keyword for a variable
+// when you create a variable you always need to start with 'var'
+// OR use 'let' or 'const' BUT these have special contextual usage.
+
+// var is a keyword- meaning it is a word built into the Javascript
+// language. We cannot use keywords in anyway other
+// than
+
+
+var userAccount = 'Nolan'
+var userAddress = '123 main street'
+
+
+var userPhoneNumberPrimary = 215-222-4445
+var userPhoneNumberSecondary = 444-235-7898
+
+let='example' // special usage- data is expected to change.
+
+const= 'example' // special usage- data is expected to NOT change.
+
+// camal case = every word after the first letter of the first word
+// is capitalized.
+tiktokAccountName= 'Nol123'
+
+// snake case = every word is separated by an underscore.
+tiktok_account_name= 'NollerRoller27'
+
+// pascal case = every words first letter is capitalized.
+TiktokAccount= 'Meowscals777'
+
+
+
+
+
---
+++ operator Review.js
@@ -0,0 +1,40 @@
+//Arithmetic operators - Writing code expressions
+// that performs math operations
+
+// examples of apps that use arithmetic operators
+
+// depositing oney into  a digital account
+var depositAmount = 50
+var checkingAccount = 1000
+var savingsAccount = 2000
+
+console.log(depositAmount + checkingAccount) 
+// function- set of instructions that performs a job
+
+//DEMOSTRATION OF NEXT WEEK
+function bankapp(userSelection) {
+if(userSelection =='deposit_into_checking') {
+   depositAmount + checkingAccount;
+  }
+}
+// peer to peer money transfers
+var amountBeingSent ='';
+var userAccountTotal ='';
+
+//Assignment - giving variables values.
+// we use the single equal sign (=) to assign data to variables
+
+// sneakers is the variable. We are ASSIGNING the value of 200.00
+// with the equal sign.
+var sneakers = 200.00
+sneakers *= .30
+
+console.log(sneakers)
+
+// Comparision - analyzing values. checking to see if a certain
+// condition is met.
+// ==
+// ===
+// !=
+
+//Logical
