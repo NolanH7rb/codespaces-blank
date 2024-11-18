df8bdc4e91930fafa9399d8579eebe17a8557ada
NolanH7rb <nhall255@boyslatin.net>
2024-11-18 18:56:32 +0000

    Asynce Activity / asynce review (Was Absent 11/11)

---
+++ Unit3_javascript/asyncActivityNov11.js
@@ -0,0 +1,53 @@
+//#1
+//A company is developing a security system that requires 2 factor authorization.
+// This means that the system needs to verify that 2 pieces of data are correct for the person to enter the building.
+// When someone approached the building they need to have the correct name and correct passcode to enter the building.
+// Which operator would be used here? "logical operator"
+//Please provide a code example of how you would write this and output the result using the print() function for python or the console.log() function for javascript?
+var username='Nolan'
+var password ='ABC123'
+
+var securitySystemUsername = 'Nolan'
+var securitySystemPassword = 'ABC123'
+
+console.log(username == securitySystemUsername)
+
+'Logical Operator'
+
+//Hint the value will need to return true
+
+//#2
+//A hospital needs to keep track of medical equipment.
+// they are getting a shipment of new ECG machines and Oxygen tanks and need to verify.
+// If they have more than whats needed in their office, they need to send the overflow of equipment back to the manufacturer, but if they have less, the need to request more.
+// In this scenario, they are supposed to have 10 ECG machines, but only recieved 4, and for the oxygen tanks they are supposed to have 6 in stock, but recieved 9.
+// Which operator would be used here? "comparison operator"
+// Please provide a code example of how you would write this and output the result using the print() function for python or the console.log()
+// function for javascript? 
+var ecgOrderRequest = 3
+var ecgShipmentRecieved = 20
+'Comparison Operator'
+
+//#3
+//A company is developing a messaging app that allows people to send text message for free. 
+//They need to allow users to capture the user data and then send it to someone else. 
+//Which operator would be used here? "assignment operator" 
+// Please provide a code example of how you would write this and output the result using the print() function for python or the console.log() function for javascript?
+var messageToSend= 'hello class'
+var messageToBeRecieved= messageToSend
+'Assignment Operator'
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////.
+
+//Identify and explain what each of these symbols are
+//    =
+'single equal sign: assignment operator'
+
+//    &&
+'double amperand: logical AND operator'
+
+//    ||
+'double vertical slash: logical OR operator '
+
+//    ==
+'double equal: comparison SAME/EQUAL operator'
+
