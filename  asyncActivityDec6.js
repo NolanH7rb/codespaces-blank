549da3923d66430aad0f6f4c6aa4040afc827ece
NolanH7rb <nhall255@boyslatin.net>
2024-12-09 19:01:23 +0000

    finished asynce activity Dec 6

---
+++  asyncActivityDec6.js
@@ -0,0 +1,26 @@
+// Question 1 -
+originalStr = "RaisingCanes";
+reversedStr = "senaCgnisiaR";
+
+for (int i = 0; i < originalStr.length(); i++) {
+  reversedStr = originalStr.charAt(i) + reversedStr;/}
+
+System.out.println("Reversed string: "+ reversedStr);
+
+
+
+
+
+
+
+
+// Question 2 - 
+function statedLandmark(landmark) {
+    let State = 'Pennsylvania';
+    if (landmark != State) {
+        console.log("");
+     } else {
+        console.log('Here with holds the Liberty Bell to represent peace of the state and unity');
+     }
+    }
+    statedLandmark('Pennsylvania')
