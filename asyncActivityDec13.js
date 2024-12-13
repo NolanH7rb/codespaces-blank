793f818e26e06670ffcf30f4c58f98a55d06f265
NolanH7rb <nhall255@boyslatin.net>
2024-12-13 20:12:47 +0000

    async work walked throughed with teacher

---
+++ asyncActivityDec13.js
@@ -0,0 +1,18 @@
+function PositiveorNegative(number) {
+        console.log('code is working');
+        console.log(number);
+        if (number >=1) {
+        console.log('this is a positive number')
+     } else if(number < 0){
+            console.log('this is a negative number')
+        } else if(number==0){
+            console.log('this is the number 0.(neutral)')
+        } else{
+            console.log('sorry you cant do that.')
+        }
+    }
+    
+    
+    
+    PositiveorNegative(7)
+    
