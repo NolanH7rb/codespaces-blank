4141f018c515a5128adbd3b8de1b8c86c898108c
NolanH7rb <nhall255@boyslatin.net>
2025-01-08 20:40:37 +0000

    COmpleted quarter 2 quiz

---
+++ Quarter2_Quiz.js
@@ -0,0 +1,73 @@
+//What is an integer data type? Please write your response in a complete sentence as a string data type.
+  "An interger is a piece or puzzle piece of data that represent numbers."
+
+//In your own words, describe what an if/ else conditional statement is. Please write your response in a complete sentence as a string data type.
+  "An if/else conditional statment is a command that involves giving a set of instructions and is used as a exit condition without having to pass in any data"
+
+//In your own words, describe what a function is. Please write your response in a complete sentence as a string data type.
+  "A function is a commands main title in a way that starts most codes."
+
+//What is a boolean data type? Please write your response in a complete sentence as a string data type.
+  "A boolean is a true or false data output"
+
+//In your own words, describe the difference between a function parameter and a function argument. Please write your response in a complete sentence as a string data typee.
+  //A function parameter is the start of a function command and a function argument is the information within the brackets of the functuion command himself.
+
+//Which operator would work best for the following function: An digital locker function that needs to have a specific set of numbers to be entered in order to lock
+//and unlock a locker in a gym. Please write your answer as a string.
+"This is the Logical Operator"
+
+//Which operator would work best for the following function: A height verification function that needs to check if a user is a certain height to get onto an amusement park ride.
+//Please write your answer as a string.
+"This is the Comparison Operator"
+
+//You have been hired as an engineer to develop a sales tax function for the state of Pennsylvania. They would like to be able to take in a annual ammount of money businesses
+//in the state make and deduct the proper amount of taxes from the their earnings. The state of PA should take 7% of a companies earnings. 
+//Your function should take in a total sales amount as an argument and return the amount thats owed to the state.
+
+//8
+
+
+
+
+
+//9
+
+function bankapp(userTaxes) {
+    if(userTaxes =='deposit_into_checking') {
+       amountBeingDuducted - userAccountTotal;
+      }
+    }
+    // peer to peer money transfers
+    var amountBeingDuducted ='14';
+    var userAccountTotal ='200';
+
+
+
+
+
+
+
+
+
+
+//10
+
+function CollegeRecomendations(numberofletters) {
+    console.log('code is working');
+    console.log(numberofletters);
+    if (numberofletters ==1) {
+        console.log('Conratulations you were accepted into Temple University and CheyNey University')
+    } else if (numberofletters ==2) {
+        console.log('Congratulations you were Accepted into Pennstate and University of Penn')
+    } else if (numberofletters ==0) {
+        console.log('sorry you werent accepted into any college institutions please try again later')
+    }
+}
+  CollegeRecomendations(2)
+
+
+
+
+
+
