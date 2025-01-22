18f92cff85e28245f90014128c742362a2d2c869
NolanH7rb <nhall255@boyslatin.net>
2025-01-22 20:17:04 +0000

    learned loops

---
+++ Unit 4/whileLoop.js
@@ -0,0 +1,13 @@
+// Loops = A loop is a special type of
+// loop where so long as a condition is TRUE
+// it will repeat the code instructions over and over.
+
+var x = 2 // this is an ex of an assignment
+// operator - giving a variable a value
+
+// = : assignment operator
+// == : comparison operator (same as)
+// != : comparison operator (not the same as)
+while (x == 2){
+    console.log('This message will repeat forever.')
+}
