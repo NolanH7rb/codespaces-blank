f919f83a4afb0b0e3c20fff40440f6cbfb250509
NolanH7rb <nhall255@boyslatin.net>
2025-01-23 20:14:51 +0000

    continued to learn about loops and intervals

--- Unit 4/whileLoop.js
+++ Unit 4/whileLoop.js
@@ -11,3 +11,25 @@ var x = 2 // this is an ex of an assignment
 while (x == 2){
     console.log('This message will repeat forever.')
 }
+
+
+
+
+
+
+10 == 10 && 2<4
+
+function timer(){
+    var interval = 60
+    while (interval > 0){
+        console.log('countdown' + interval)
+        interval -=1
+    
+        
+        document.getElementaryById('count').innerHTML
+          alert('times up')
+           console.log('Times up!')
+            
+    }
+}
+ 
