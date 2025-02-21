b9bfb0409337d16f305c4716cdaf863a33534fd6
NolanH7rb <nhall255@boyslatin.net>
2025-02-21 20:10:24 +0000

    Completed async activity about game idea

---
+++ Unit 5/asyncActivityFeb21.js
@@ -0,0 +1,15 @@
+// 1.  ZOMBIE OBJECTION
+
+// 2. horror
+
+// 3. Players will score by shooting zombie and/or zombie heads
+//  falling from the sky in order win, although with the misclick
+//  of a survivor coming for rescue the game will end.
+
+// 4. This game is similar to r6 protect the hostage, walking dead,
+//  and zombie raft. These games are going to be reflected in my game
+//  by the dark and gory theme of the game.
+
+// 5. - My game will allow players to use their mous to aim horizontally and vertically
+//    - My game will keep score of the zombies eliminated and hostages saved
+//    - My game will will have 3 different weapon choices to choose from
